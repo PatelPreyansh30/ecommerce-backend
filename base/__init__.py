bf17967711e817f81bb6dc29bd601df246a4c0ef
Preyansh <patelpreyansh3011@gmail.com>
2023-01-27 20:48:24 +0530

    code setup

--- base/__init__.py
+++ base/__init__.py
@@ -1,18 +1,13 @@
-from base.com import controller
 from datetime import timedelta
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'preyansh'
+app.secret_key = 'e_commerce_app'
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/dashboard'
-
-app.config['SQLALCHEMY_ECHO'] = False
-
-app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
 
 db = SQLAlchemy(app)
 
--- base/com/controller/home_controller.py
+++ base/com/controller/home_controller.py
@@ -1,6 +1,7 @@
+from flask import *
 from base import app
 
 
-@app.route('/', ['GET'])
+@app.route('/', methods=['GET'])
 def get_home_page():
     return "Hello World"
