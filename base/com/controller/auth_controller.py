bdca9efdd1506f7b972706c75a6e2df4684a8772
Preyansh <patelpreyansh3011@gmail.com>
2023-01-28 16:55:11 +0530

    getting access token from refresh token

--- base/com/controller/auth_controller.py
+++ base/com/controller/auth_controller.py
@@ -1,9 +1,9 @@
 from flask import request, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from base import app
-from base.com.dao.auth_dao import UserDAO
-from base.com.vo.auth_vo import UserVO
+from base.com.dao.auth_dao import UserDAO, RefreshTokenDAO
+from base.com.vo.auth_vo import UserVO, RefreshTokenVO
 
 @app.route('/api/a1/auth/register', methods=['POST'])
 def auth_register():
@@ -11,7 +11,7 @@ def auth_register():
     password = request.json.get('password')
         
     if not email or not password:
-        return make_response({"message": "Invalid data", "statusCode": 400}, 400)
+        return make_response({"message": "Invalid data"}, 400)
     else:
         user_dao = UserDAO()
         user_vo = UserVO()
@@ -22,10 +22,10 @@ def auth_register():
         try:
             existing_user = user_dao.get_single_user(email)
             user_dao.add_user(user_vo)
-            return make_response({"message": "Successfully added", "statusCode": 201}, 201)
+            return make_response({"message": "Successfully added"}, 201)
             
         except Exception as e:
-            return make_response({"message": "Email already exists", "statusCode": 400}, 400)
+            return make_response({"message": "Email already exists"}, 400)
 
 @app.route('/api/a1/auth/login', methods=['POST'])
 def auth_login():
@@ -33,7 +33,7 @@ def auth_login():
     password = request.json.get('password')
     
     if not email or not password:
-        return make_response({"message": "Invalid data", "statusCode": 400}, 400)
+        return make_response({"message": "Invalid data"}, 400)
     else:
         user_dao = UserDAO()
 
@@ -44,15 +44,26 @@ def auth_login():
                 
                 access_token = create_access_token(identity=email)
                 refresh_token = create_refresh_token(identity=email)
+                refresh_token_dao = RefreshTokenDAO()
+                refresh_token_vo = RefreshTokenVO()
                 
-                return make_response({"accessToken": access_token, "refreshToken": refresh_token, "statusCode": 201}, 201)
+                refresh_token_vo.refresh_token = refresh_token
+                refresh_token_vo.refresh_user_id = existing_user.user_id
+                
+                try:
+                    refresh_token_dao.add_refresh_token(refresh_token_vo)
+                    return make_response({"accessToken": access_token, "refreshToken": refresh_token}, 201)
+                except:
+                    return make_response({"message": "Some error occured while storing the refresh token"}, 400)    
             else:
-                return make_response({"message": "Invalid credential", "statusCode": 400}, 400)
+                return make_response({"message": "Invalid credential"}, 400)
             
         else:
-            return make_response({"message": "User doesn't exists", "statusCode": 400}, 400)
+            return make_response({"message": "User doesn't exists"}, 400)
 
-# @app.route('/api/a1/auth/get-access-token', methods=['POST'])
-# @jwt_refresh_token_required
-# def get_access_token():
-#     return 
+@app.route('/api/a1/auth/get-access-token', methods=['POST'])
+@jwt_required(refresh=True)
+def get_access_token():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity)
+    return make_response({"accessToken": access_token}, 201)
--- base/com/dao/auth_dao.py
+++ base/com/dao/auth_dao.py
@@ -10,23 +10,9 @@ class UserDAO():
         user_object = UserVO.query.filter_by(user_email = user_email).first()
         return user_object
     
-    # def view_subcategory(self):
-    #     subcategory_vo_list = db.session.query(SubCategoryVO, CategoryVO).join(CategoryVO,SubCategoryVO.subcategory_category_id == CategoryVO.category_id).all()
-    #     return subcategory_vo_list
 
-    # def delete_subcategory(self, subcategory_id):
-    #     subcategory_vo_obj = SubCategoryVO.query.get(subcategory_id)
-    #     db.session.delete(subcategory_vo_obj)
-    #     db.session.commit()
+class RefreshTokenDAO():
+    def add_refresh_token(self, refresh_token_obj):
+        db.session.add(refresh_token_obj)
+        db.session.commit()
     
-    # def edit_subcategory(self, subcategory_vo):
-    #     subcategory_vo_obj = SubCategoryVO.query.filter_by(subcategory_id = subcategory_vo.subcategory_id).first()
-    #     return subcategory_vo_obj
-    
-    # def update_subcategory(self, subcategory_vo):
-    #     db.session.merge(subcategory_vo)
-    #     db.session.commit()
-
-    # def get_subcategory_by_category_id(self, category_id):
-    #     subcategory = SubCategoryVO.query.filter_by(subcategory_category_id = category_id)
-    #     return subcategory
