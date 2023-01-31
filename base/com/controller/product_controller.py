3e092886cb8de39b77a57d2c9b7033d4c8e52624
Rushikeshf01 <falakrushikesh83@gmail.com>
2023-01-31 23:42:07 +0530

    routes and jwt applied

--- base/com/controller/product_controller.py
+++ base/com/controller/product_controller.py
@@ -1,11 +1,13 @@
 from base import app
 from base.com.vo.product_vo import ProductVO
 from base.com.dao.product_dao import ProductDAO
-from flask import jsonify,make_response
+from flask_jwt_extended import jwt_required
+from flask import make_response
 
-@app.route('/api/a1/products')
+@app.route('/api/a3/products')
+@jwt_required()
 def get_products():
     product_dao = ProductDAO()
     prod_obj_list = product_dao.get_all_products()
     all_products = list(map(ProductVO.as_dict, prod_obj_list))
-    return make_response({"products":f"{all_products}"}, 200)
+    return make_response({"products":all_products}, 200)
--- base/com/vo/product_vo.py
+++ base/com/vo/product_vo.py
@@ -1,6 +1,6 @@
 from base import db,app
 
-class Product_categoryVO(db.Model):
+class ProductCategoryVO(db.Model):
     __tablename__ = 'product_category'
     product_category_id = db.Column('product_category_id', db.Integer, primary_key = True,autoincrement = True)
     product_category_name = db.Column('product_category_name', db.String(250), nullable = False)
@@ -15,7 +15,7 @@ class Product_categoryVO(db.Model):
         }
 
 
-class Product_inventoryVO(db.Model):
+class ProductInventoryVO(db.Model):
     __tablename__ = 'product_inventory'
     product_inventory_id = db.Column('product_inventory_id', db.Integer, primary_key = True,autoincrement = True)
     product_inventory_quantity = db.Column('product_inventory_quantity', db.Integer)
@@ -34,13 +34,9 @@ class ProductVO(db.Model):
     product_name = db.Column('product_name', db.String(250), nullable = False)
     product_description = db.Column('product_description', db.Text, nullable=False)
     product_price = db.Column('product_price', db.Float, nullable=False)
-    product_category_id = db.Column('product_category_id', db.ForeignKey(Product_categoryVO.product_category_id, ondelete= 'CASCADE', onupdate= 'CASCADE'), nullable= False, unique=True)
-    product_inventory_id = db.Column('product_inventory_id', db.ForeignKey(Product_inventoryVO.product_inventory_id, ondelete= 'CASCADE', onupdate= 'CASCADE'), nullable= False, unique=True)
+    product_category_id = db.Column('product_category_id', db.ForeignKey(ProductCategoryVO.product_category_id, ondelete= 'CASCADE', onupdate= 'CASCADE'), nullable= False, unique=True)
+    product_inventory_id = db.Column('product_inventory_id', db.ForeignKey(ProductInventoryVO.product_inventory_id, ondelete= 'CASCADE', onupdate= 'CASCADE'), nullable= False, unique=True)
 
-    # def serialize(self):
-    #     return {"id": self.id,
-    #             "product_name": self.product_name,
-    #             "product_description": self.product_description}
             
     def as_dict(self):
         return {
