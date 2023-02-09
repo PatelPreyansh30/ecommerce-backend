dca3772f3dac7b8bd4b3c2ad943df2deb3baad82
Preyansh <patelpreyansh3011@gmail.com>
2023-02-09 10:28:44 +0530

    user info model added

--- base/com/controller/user_controller.py
+++ base/com/controller/user_controller.py
@@ -5,9 +5,22 @@ from base.com.dao.product_dao import ProductReviewsRatingsDAO
 
 user_api_path = '/api/a4/user'
 
+
 @app.route(f'{user_api_path}/reviews')
 @jwt_required()
 def get_reviews_by_user():
+    product_review_dao = ProductReviewsRatingsDAO()
+    user_id = get_jwt_identity().get('userId')
+    data = product_review_dao.get_reviews_ratings_by_user(user_id)
+    if len(data) != 0:
+        return make_response({"reviews": data}, 200)
+    else:
+        return make_response({"msg": "No reviews found"}, 400)
+
+
+@app.route(f'{user_api_path}/profile')
+@jwt_required()
+def get_user_profile():
     product_review_dao = ProductReviewsRatingsDAO()
     user_id = get_jwt_identity().get('userId')
     data = product_review_dao.get_reviews_ratings_by_user(user_id)
---
+++ base/com/vo/user_vo.py
@@ -0,0 +1,35 @@
+import datetime
+from base import db, app
+from base.com.vo.auth_vo import UserVO
+
+
+class UserInfoVO(db.Model):
+    __tablename__ = 'user_table'
+    user_info_id = db.Column('user_info_id', db.Integer,
+                             primary_key=True, autoincrement=True)
+    user_first_name = db.Column('user_first_name', db.String(255),
+                                nullable=False)
+    user_last_name = db.Column('user_last_name', db.String(255),
+                               nullable=False)
+    user_dob = db.Column('user_dob', db.Date,
+                         nullable=False)
+    user_mobile = db.Column('user_mobile', db.String(
+        255), nullable=False, unique=True)
+    user_id = db.Column('user_id', db.ForeignKey(
+        UserVO.user_id, ondelete='CASCADE', onupdate='CASCADE'), nullable=False, unique=True)
+    created_at = db.Column('created_at', db.DateTime,
+                           default=datetime.datetime.utcnow)
+    updated_at = db.Column('updated_at', db.DateTime,
+                           default=datetime.datetime.utcnow)
+
+    def as_dict(self):
+        return {
+            'firstName': self.user_first_name,
+            'lastName': self.user_last_name,
+            'dob': self.user_dob,
+            'mobile': self.user_mobile,
+        }
+
+
+with app.app_context():
+    db.create_all()
