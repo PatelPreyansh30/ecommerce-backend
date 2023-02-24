1941b937f120c6a798c479f9c305f7d3c3cdb26a
Preyansh <patelpreyansh3011@gmail.com>
2023-02-24 22:21:20 +0530

    post api added for cart

--- base/com/controller/order_controller.py
+++ base/com/controller/order_controller.py
@@ -16,7 +16,21 @@ def get_cart_by_user():
     if request.method == 'GET':
         try:
             cart = cart_dao.get_user_cart(user_id)
+            if len(cart) == 0:
+                return make_response({'msg': "No cart found"}, 400)
             return make_response({'cart': cart}, 200)
+        except Exception as e:
+            return make_response({'msg': "Something went wrong"}, 400)
+
+    if request.method == 'POST':
+        try:
+            product_id = request.json.get('productId')
+            quantity = request.json.get('quantity')
+
+            res = cart_dao.post_product_in_cart(product_id, quantity, user_id)
+            if not res:
+                return make_response({'msg': 'Product already added'}, 400)
+            return make_response({'cart': res, 'msg': 'Product added in cart'}, 201)
         except Exception as e:
             print(e)
             return make_response({'msg': "Something went wrong"}, 400)
--- base/com/dao/order_dao.py
+++ base/com/dao/order_dao.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from base import db
 from base.com.vo.order_vo import OrderDetailVO, OrderItemVO, CartVO
 from base.com.vo.product_vo import ProductVO
@@ -8,12 +8,30 @@ class CartDAO():
     def get_user_cart(self, user_id):
         cart = db.session.query(CartVO, ProductVO).join(
             ProductVO, CartVO.product_id == ProductVO.product_id
-        ).filter(CartVO.user_id==user_id).all()
-        print(cart)
+        ).filter(CartVO.user_id == user_id).all()
         data = []
         for item in cart:
             cart_object = {}
             cart_object.update(item[0].as_dict())
             cart_object.update(item[1].as_dict())
             data.append(cart_object)
+        return data
+
+    def post_product_in_cart(self, product_id, quantity, user_id):
+        cart_obj = CartVO(
+            quantity=quantity,
+            user_id=user_id,
+            product_id=product_id
+        )
+        db.session.add(cart_obj)
+        cart = db.session.query(CartVO, ProductVO).join(
+            ProductVO, CartVO.product_id == ProductVO.product_id
+        ).filter(CartVO.user_id == user_id, CartVO.product_id == product_id).all()
+
+        if len(cart) > 1:
+            return None
+        data = {}
+        for item in cart[0]:
+            data.update(item.as_dict())
+        db.session.commit()
         return data
