08148e7483c70617df3f53e4d7bf89eeceb3eb8e
Preyansh <patelpreyansh3011@gmail.com>
2023-02-25 21:28:33 +0530

    added subcategory name in cart api response

--- base/com/dao/order_dao.py
+++ base/com/dao/order_dao.py
@@ -1,19 +1,22 @@
 from datetime import datetime
 from base import db
 from base.com.vo.order_vo import OrderDetailVO, OrderItemVO, CartVO
-from base.com.vo.product_vo import ProductVO
+from base.com.vo.product_vo import ProductVO, ProductSubCategoryVO
 
 
 class CartDAO():
     def get_user_cart(self, user_id):
-        cart = db.session.query(CartVO, ProductVO).join(
+        cart = db.session.query(CartVO, ProductVO, ProductSubCategoryVO).join(
             ProductVO, CartVO.product_id == ProductVO.product_id
+        ).join(
+            ProductSubCategoryVO, ProductVO.subcategory_id == ProductSubCategoryVO.subcategory_id
         ).filter(CartVO.user_id == user_id).all()
         data = []
         for item in cart:
             cart_object = {}
             cart_object.update(item[0].as_dict())
             cart_object.update(item[1].as_dict())
+            cart_object.update(item[2].as_dict())
             data.append(cart_object)
         return data
 
@@ -24,8 +27,10 @@ class CartDAO():
             product_id=product_id
         )
         db.session.add(cart_obj)
-        cart = db.session.query(CartVO, ProductVO).join(
+        cart = db.session.query(CartVO, ProductVO, ProductSubCategoryVO).join(
             ProductVO, CartVO.product_id == ProductVO.product_id
+        ).join(
+            ProductSubCategoryVO, ProductVO.subcategory_id == ProductSubCategoryVO.subcategory_id
         ).filter(CartVO.user_id == user_id, CartVO.product_id == product_id).all()
 
         if len(cart) > 1:
